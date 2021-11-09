4aef8367fb96919c0289776ebc267eb9118311bb
kverenev <kverenev@gmail.com>
2021-11-09 17:12:29 +0300

    add react-navigation v4

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
-const CategoryMealsScreen = () => {
+const CategoryMealsScreen = (props) => {
   return (
     <View style={styles.screen}>
       <Text>The Category Meals Screen</Text>
+      <Button title={'Go to one concrete Meal!'} onPress={() => { props.navigation.navigate('MealDetail'); }} />
     </View>
   );
 };
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
-const MealDetailScreen = () => {
+const MealDetailScreen = (props) => {
   return (
     <View style={styles.screen}>
       <Text>The Meal Detail Screen</Text>
+      <Button title={'Go back'} onPress={() => { props.navigation.pop(); }} />
     </View>
   );
 };
