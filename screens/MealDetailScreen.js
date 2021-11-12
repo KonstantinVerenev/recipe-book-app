0c5c0f3ad6fe08fe8d9b61815a7375ccb0c0586f
kverenev <kverenev@gmail.com>
2021-11-12 20:14:25 +0300

    add react-navigation-header-buttons v6 27

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { MEALS } from '../data/data';
+import CustomHeaderButton from '../components/CustomHeaderButton';
 
 const MealDetailScreen = (props) => {
   const mealId = props.navigation.getParam('mealId');
@@ -22,6 +24,20 @@ MealDetailScreen.navigationOptions = (navigationData) => {
 
   return {
     headerTitle: selectedMeal.title,
+    headerRight: (() => {
+      return (
+        <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+          <Item
+            title="Favorite"
+            iconName="ios-star"
+            onPress={() => {
+              console.log('Mark as favorite!');
+            }}
+          />
+        </HeaderButtons>
+      );
+    }
+    )
   };
 };
 
